a334c9931e5ede2a5397ada61edd8e9002d7f9d2
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-12 12:19:04 +0200

    [snack 2 completed]

--- snack.js
+++ snack.js
@@ -2,7 +2,12 @@ function getInitials(nomeCompleto) {
     const [nome, cognome] = nomeCompleto.split(" ");
     return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`
 }
+function createSlug(titolo) {
+    return titolo.toLowerCase();
+}
+
 
 module.exports = {
-    getInitials
+    getInitials,
+    createSlug
 }
