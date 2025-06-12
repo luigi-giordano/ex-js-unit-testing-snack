a6725ff457d59943875dc443f463a08d149740cc
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-12 13:04:34 +0200

    [snack 3 completed]

--- snack.js
+++ snack.js
@@ -2,12 +2,23 @@ function getInitials(nomeCompleto) {
     const [nome, cognome] = nomeCompleto.split(" ");
     return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`
 }
+
 function createSlug(titolo) {
     return titolo.toLowerCase();
+}
+
+function average(numeri) {
+    numeri.forEach(numero => {
+        if (isNaN(numero)) {
+            throw new Error("Average accetta solo numeri");
+        }
+    });
+    return numeri.reduce((acc, numero) => acc + numero, 0) / numeri.length;
 }
 
 
 module.exports = {
     getInitials,
-    createSlug
+    createSlug,
+    average
 }
--- snack.test.js
+++ snack.test.js
@@ -1,11 +1,15 @@
-const { getInitials, createSlug } = require("./snack.js");
+const { getInitials, createSlug, average } = require("./snack.js");
 
 test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
     expect(getInitials("Mario Rossi")).toBe("M.R.");
     expect(getInitials("mario rossi")).toBe("M.R.");
 });
 
-
 test('La funzione createSlug restituisce una stringa in lowercase.', () => {
     expect(createSlug("Stringa coN maIuscolE")).toBe("stringa con maiuscole")
+});
+
+test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+    expect(average([5, 15])).toBe(10);
+    expect(() => average([5, "hello"])).toThrow();
 });
