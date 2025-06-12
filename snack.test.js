01fe120284aae9db8661e878f61d6c718f117f77
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-12 12:21:10 +0200

    [snack 2 completed][fix]

--- snack.test.js
+++ snack.test.js
@@ -1,8 +1,11 @@
-const { getInitials } = require("./snack.js");
-
-
+const { getInitials, createSlug } = require("./snack.js");
 
 test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
     expect(getInitials("Mario Rossi")).toBe("M.R.");
     expect(getInitials("mario rossi")).toBe("M.R.");
+});
+
+
+test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+    expect(createSlug("Stringa coN maIuscolE")).toBe("stringa con maiuscole")
 });
